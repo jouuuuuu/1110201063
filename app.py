860787f83e7544d0828803d5725fb949b07db6b5
jouuuuuu <95198285+jouuuuuu@users.noreply.github.com>
2021-12-13 11:45:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ import random
 app = Flask(__name__)
 
 
-line_bot_api = LineBotApi('')
+line_bot_api = LineBotApi('6LQfuCZix17rWj5hVVK2nKQNcboB5rU1oQh5ET6ZqZ4CDpBhvSAwGHKk4ecWsKYYs8xCTmG03TLeLgxck17MeNiNcYlg2XFMOZgU7duukXZBw9Zs4/kAqbjRdvFCNNQ274ssaPUg4Un3MRAfG1EvugdB04t89/1O/w1cDnyilFU=')
 
-handler = WebhookHandler('')
+handler = WebhookHandler('fee10142b286079ac4482862e815c912')
 
-line_bot_api.push_message('', TextSendMessage(text='系統測試中，若您覺得訊息干擾到您，您可以將聊天室設為靜音，謝謝喔！'))
+line_bot_api.push_message('U91a40e1391d3bae1c771cf114ceaf3b9', TextSendMessage(text='系統測試中，若您覺得訊息干擾到您，您可以將聊天室設為靜音，謝謝喔！'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
